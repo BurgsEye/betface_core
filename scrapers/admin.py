614feb7d88f62e9b2850c10ea89d33f7457e4168
Tony Forester <tony@Tonys-MacBook-Air.local>
2023-08-29 22:50:41 +0100

    did something funky with the django admin page

--- scrapers/admin.py
+++ scrapers/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import ScraperLog
 # Register your models here.
 
-admin.site.register(ScraperLog)
+
+class ScraperLogAdmin(admin.ModelAdmin):
+    list_display = ('scraper_name', 'scraper_start_time', 'error_level', 'scraper_message')
+    list_filter = ('scraper_name', 'scraper_start_time', 'error_level')
+    search_fields = ('scraper_name', 'scraper_start_time', 'error_level')
+
+admin.site.register(ScraperLog, ScraperLogAdmin)
